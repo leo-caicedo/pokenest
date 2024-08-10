b96fbe3d475b45414d23f73818e5f76d4934147c
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2024-08-09 23:56:53 -0500

    crear metodo seed de pokemons

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import axios, { AxiosInstance } from 'axios';
+import { Model } from 'mongoose';
+
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { PokeResponse } from './interfaces/poke-response.interface';
+
+@Injectable()
+export class SeedService {
+  private readonly axios: AxiosInstance = axios
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ) {}
+
+  async sowDb() {
+    await this.pokemonModel.deleteMany({})
+
+    const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650')
+
+    const pokemonToInsert: {name: string, no: number}[] = []
+
+    data.results.forEach(({name, url}) => {
+      const segments = url.split('/')
+      const no = +segments[segments.length -2]
+
+      pokemonToInsert.push({ name, no })
+    })
+
+    await this.pokemonModel.insertMany(pokemonToInsert)
+
+    return data.results
+  }
+}
